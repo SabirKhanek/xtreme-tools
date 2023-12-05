3357d23676c215d98dbca7d43b91244006135972
Sabir Khan <sabirkhanek66@gmail.com>
2023-12-05 18:26:46 +0500

    fix: error

--- src/services/ai.ts
+++ src/services/ai.ts
@@ -1,7 +1,6 @@
 import { axios } from "../shared/axios";
 
 export async function generateContent(input: string) {
-  if (input.length <= 5) return;
   const resp = await axios.get(`/ai/write?userInput=${input}`);
   return resp.data;
 }
