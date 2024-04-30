892b345bea3572464ea2d6ba5fe407e07439c3b6
Sabir Khan <sabirkhanek66@gmail.com>
2024-04-30 19:42:40 +0500

    try

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -119,10 +119,8 @@ function BlogCard({ blog }: { blog: blogs }) {
     <div className="bg-primary/5 h-full p-5 ">
       <div className="border border-black/70 rounded-lg w-full  overflow-hidden aspect-[310/179]">
         <img
-          width={310}
-          height={179}
           alt={"blog thumbnail"}
-          className="object-center object-cover"
+          className="w-full h-full object-center object-cover"
           src={image || "/assests/noimage.png"}
         ></img>
       </div>
